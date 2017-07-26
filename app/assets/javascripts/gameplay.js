eaec0f9504a356a3443d3d391d5c3e088df028d8
mlsayre <mlsayre@gmail.com>
2017-07-26 11:48:29 -0700

    first hint submit

--- app/assets/javascripts/gameplay.js
+++ app/assets/javascripts/gameplay.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
 
 	var hint1 = "";
+	var hint1num = 0;
+	var hint2 = "";
+	var hint2num = 0;
+
 	$(".submithint1").click(function() {
 		var enteredtext = $(".hint1word").val();
-		validate(enteredtext);
-	})
+		var val = validate(enteredtext);
+		if (val === false) {
+			return false;
+		}
+		console.log("successful entry")
+		var entered
+		hint1 = enteredtext.toUpperCase();
+		hint1num = parseInt($(".hint1number").val())
+		submithint1(hint1, hint1num);
+	});
 
 	function validate(word) {
+		var letters = /^[a-zA-Z\s]+$/;
+		var numberofwords = word.trim().split(/\s+/).length
 		if (word.length === 0) {
-			errorbox("Please enter a hint word.")
-		} else if (word.length > 22) {
-			errorbox("Please keep hint shorter than 23 characters. Reminder: just give one word or two words if it's somebody's name.")
+			errorbox("Please enter a hint word.");
+			return false;
+		} else if (word.length > 28) {
+			errorbox("Please keep hint shorter than 29 characters. Reminder: just give one word or two words if it's somebody's name.");
+			return false;
+		} else if (!word.match(letters)) {
+			errorbox("Please make sure your hint contains letters only.");
+			return false;
+		} else if (numberofwords > 2) {
+			errorbox("Please make sure your hint is one word only, or two words if using somebody's name.");
+			return false;
 		}
 	}
 
@@ -19,6 +41,29 @@ $(document).ready(function() {
 		$(".messageinfo").text(message);
 		$(".messageaction").html('<button class="button closemessagebox">Return to game</button>');
 		$(".messagesubtext").text("Push button or click anywhere outside this box to continue.");
+		$(".closemessagebox").click(function() { closemessagebox(); });
+		$(".pagecover").click(function() { closemessagebox(); });
+		$(".messagebox").show();
+		$(".pagecover").show();
+	}
+
+	function submithint1(hint, num) {
+		if (num === 1) {
+			var wordword = "word";
+		} else {
+			wordword = "words"
+		}
+		$(".messagetitle").text("Submit Hint?")
+		$(".messageinfo").html('You have entered the hint "' + hint + '" which applies to <bold>' +
+		                        num + '</bold> ' + wordword + '. Submit this hint and move to the final hint?');
+		$(".messageaction").html('<button class="button submithint1final">Submit Hint 1</button>' +
+			                       '<button class="button closemessagebox">Cancel</button>');
+		$(".messagesubtext").text("Push cancel or click anywhere outside this box to cancel.");
+		$(".submithint1final").click(function() {
+			$(".hint1").hide();
+			$(".hint2").show()
+			closemessagebox();
+		})
 		$(".closemessagebox").click(function() { closemessagebox(); });
 		$(".pagecover").click(function() { closemessagebox(); });
 		$(".messagebox").show();
@@ -34,12 +79,20 @@ $(document).ready(function() {
 		$(".messagesubtext").html("");
 	}
 
+	// handle words up to "antidisestablishmentarianism"
 	$('.hintenter').keyup(function() {
     var tlength = $(this).val().length;
-    $(this).val($(this).val().substring(0,22));
+    $(this).val($(this).val().substring(0,28));
     var tlength = $(this).val().length;
     // remain = parseInt(tlength);
     // $('#remain').text(remain);
   }); 
+
+  $('.hintenter').on("keypress", function(e) {
+  	if (e.keyCode === 13) {
+  		e.preventDefault();
+  		$(this).closest(".hints").find("button").first().click();
+  	}
+  });
 
 });
