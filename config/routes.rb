bfa1090b2358e166a9eeb9c7f998a6d9a4434666
mlsayre <mlsayre@gmail.com>
2017-09-13 00:14:54 -0700

    fixing oauth error

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # authenticated :user do
   #   root to: 'games#new', as: :authenticated_root
