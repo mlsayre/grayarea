87c9d66d406fee6b6bb66d0f3f8c81f636d7a483
mlsayre <mlsayre@gmail.com>
2020-03-23 16:53:51 -0700

    fix email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 "587",
-    domain:               'wordstretch.com',
+    domain:               'gmail.com',
     user_name:            'matt@wordstretch.com',
     password:             ENV['GM_PASS'],
     authentication:       'plain',
